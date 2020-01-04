e5993e073578fc08bb3ac9d22b3c29b8c7ebe60d
자유해결사 <khjde1207@knou.ac.kr>
2020-01-04 21:38:56 +0900

    text / textInput 의 경우 child 가 있으면 에러 발생. child 존재 여부에 따라 재귀 호출 방식 변경 .

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-import React from 'react'; 
+import React ,{ComponentType} from 'react'; 
 import _ from 'lodash'
 import { prop2styles } from './lib/cls'
 import colorObj  from './lib/colorObj'
 
 const ClsComponent = (comp)=>{
     let child = _.get(comp , "children") || _.get(comp , "props.children");
-    if(child){
+    if(child){ 
         return React.Children.map(child , childcomp=>{
             if(React.isValidElement(childcomp)){
                 let cls = prop2styles(childcomp.props)
-                let rtn = React.cloneElement(childcomp, cls , ClsComponent(childcomp)) 
+                let rtn = {};
+                let child_child = _.get(childcomp , "children") || _.get(childcomp , "props.children");
+                if(child_child){
+                    rtn = React.cloneElement(childcomp, cls , ClsComponent(childcomp)) 
+                }else{
+                    rtn = React.cloneElement(childcomp, cls) 
+                }
                 return rtn;
             }
             return  childcomp
         })
+    } 
+
+    if(React.isValidElement(comp)){
+        var cls = prop2styles(comp.props)
+        return React.cloneElement(comp, cls)  
     }
     return null;//React.cloneElement(comp, {style:{}}) 
 }
 let test =prop2styles; 
 let setColor = (id , colorCode)=>{
     // console.log(colorObj)
-    colorObj[id] = colorCode;
+    colorObj[id] = colorCode; 
     return true;
 }
 
@@ -41,7 +52,7 @@ export default (props)=>{
     // console.log(new ClsComponent(props))  
     // return ss 
     // return  <View></View>
-    // let comp = <ClsComponent {...props} ></ClsComponent>
-    // console.log(" >>: " , props.children )  
-    return  <ClsComponent {...props} ></ClsComponent>;
+    let comp = <ClsComponent {...props} ></ClsComponent>
+    // console.log(" >>>>: " , comp)   
+    return  comp;
 }
