fa663fc14ef98a7c04d25bedc698b49b83737c6f
자유해결사 <khjde1207@knou.ac.kr>
2020-01-04 20:23:49 +0900

    setColor 함수 추가 .

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import React from 'react'; 
 import _ from 'lodash'
-
-
 import { prop2styles } from './lib/cls'
+import colorObj  from './lib/colorObj'
 
 const ClsComponent = (comp)=>{
     let child = _.get(comp , "children") || _.get(comp , "props.children");
@@ -16,10 +15,33 @@ const ClsComponent = (comp)=>{
             return  childcomp
         })
     }
-    return React.cloneElement(comp, {style:{}}) 
+    return null;//React.cloneElement(comp, {style:{}}) 
+}
+let test =prop2styles; 
+let setColor = (id , colorCode)=>{
+    // console.log(colorObj)
+    colorObj[id] = colorCode;
+    return true;
+}
+
+export {
+    test,
+    setColor
 }
 
 export default (props)=>{
+    // let ss = React.Children.map(props.children , childcomp=>{
+    //     // console.log(childcomp.props.children)
+    //    let rtn =  React.cloneElement(childcomp)
+    //    console.log(rtn)
+    //    return rtn 
+    // }) 
+    // console.log(ss)
+    // let comp1 = <ClsComponent {...props} ></ClsComponent>
+    // console.log(new ClsComponent(props))  
+    // return ss 
+    // return  <View></View>
     // let comp = <ClsComponent {...props} ></ClsComponent>
+    // console.log(" >>: " , props.children )  
     return  <ClsComponent {...props} ></ClsComponent>;
 }
