df6e0c4ddf3dd17a2a0642e6fb8045a01b9c00c3
자유해결사 <khjde1207@knou.ac.kr>
2020-03-01 15:53:34 +0900

    마이너스값 지원 추가

--- index.js
+++ index.js
@@ -1,70 +1,69 @@
-import React ,{ComponentType} from 'react'; 
-import _ from 'lodash'
-import { prop2styles } from './lib/cls'
-import colorObj  from './lib/colorObj'
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import React from 'react';
+import _ from 'lodash';
+import {prop2styles} from './lib/cls';
+import colorObj from './lib/colorObj';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 
-const ClsComponent = (comp)=>{
-    let child = _.get(comp , "children") || _.get(comp , "props.children");
-     
-    if(child){ 
-        return React.Children.map(child , childcomp=>{
-            if(React.isValidElement(childcomp)){
-                let cls = prop2styles(childcomp.props)
-                let rtn = {};
-                let child_child = _.get(childcomp , "children") || _.get(childcomp , "props.children");
-                _.forEach(childcomp.props , (prop, key)=>{
-                    if(key === "children"){ return;}
-                    var bel  = React.isValidElement(prop) 
-                    if(bel){ 
-                        let clsch = prop2styles(prop.props)
-                        cls[key] = React.cloneElement(prop, clsch , ClsComponent(prop)) 
-                    }
-                });
-                if(child_child){
-                    rtn = React.cloneElement(childcomp, cls , ClsComponent(childcomp)) 
-                }else{
-                    rtn = React.cloneElement(childcomp, cls) 
-                }
-                return rtn;
-            }
-            return  childcomp
-        })
-    } 
-    
-    if(React.isValidElement(comp)){
-        var cls = prop2styles(comp.props)
-        return React.cloneElement(comp, cls)  
-    }
-    return null;//React.cloneElement(comp, {style:{}}) 
-}
-let test =prop2styles; 
-let setColor = (id , colorCode)=>{
-    // console.log(colorObj) 
-    colorObj[id] = colorCode; 
-    return true;
-}
+const ClsComponent = comp => {
+  let child = _.get(comp, 'children') || _.get(comp, 'props.children');
 
-export {
-    test,
-    setColor,
-    wp,
-    hp
-}
+  if (child) {
+    return React.Children.map(child, childcomp => {
+      if (React.isValidElement(childcomp)) {
+        let cls = prop2styles(childcomp.props);
+        let rtn = {};
+        let child_child =
+          _.get(childcomp, 'children') || _.get(childcomp, 'props.children');
 
-export default (props)=>{
-    // let ss = React.Children.map(props.children , childcomp=>{
-    //     // console.log(childcomp.props.children)
-    //    let rtn =  React.cloneElement(childcomp)
-    //    console.log(rtn)
-    //    return rtn 
-    // }) 
-    // console.log(ss)
-    // let comp1 = <ClsComponent {...props} ></ClsComponent>
-    // console.log(new ClsComponent(props))  
-    // return ss 
-    // return  <View></View>
-    let comp = <ClsComponent {...props} ></ClsComponent>
-    // console.log(" >>>>: " , comp)   
-    return  comp;
-}
+        _.forEach(childcomp.props, (prop, key) => {
+          if (key === 'children') {
+            return;
+          }
+          var bel = React.isValidElement(prop);
+          if (bel) {
+            let clsch = prop2styles(prop.props);
+            cls[key] = React.cloneElement(prop, clsch, ClsComponent(prop));
+          }
+        });
+        if (child_child) {
+          rtn = React.cloneElement(childcomp, cls, ClsComponent(childcomp));
+        } else {
+          rtn = React.cloneElement(childcomp, cls);
+        }
+        return rtn;
+      }
+      return childcomp;
+    });
+  }
+
+  if (React.isValidElement(comp)) {
+    var cls = prop2styles(comp.props);
+    return React.cloneElement(comp, cls);
+  }
+  return null; //React.cloneElement(comp, {style:{}})
+};
+let test = prop2styles;
+let setColor = (id, colorCode) => {
+  // console.log(colorObj)
+  colorObj[id] = colorCode;
+  return true;
+};
+
+let clsGet = text => {
+  let rtn = prop2styles({cls: text});
+  if (rtn.style) {
+    return rtn.style[0];
+  }
+  return rtn;
+};
+
+export {test, setColor, wp, hp, clsGet};
+
+export default props => {
+  let comp = <ClsComponent {...props} />;
+  // console.log(" >>>>: " , comp)
+  return comp;
+};
